98983c6b18fcc45fc6612eb7e3097107c2683fe2
Yukari0201 <130890393+Yukari0201@users.noreply.github.com>
2026-04-23 12:12:34 +0800

    Upload Scripts

--- scripts/fix-code.lua
+++ scripts/fix-code.lua
@@ -1,4 +1,4 @@
 function CodeBlock(e)
   -- 强制将所有代码块转为带围栏的格式，并添加语言标识
-  return pandoc.RawBlock('markdown', '```lua\n' .. e.text .. '\n```')
+  return pandoc.RawBlock('markdown', '```\n' .. e.text .. '\n```')
 end
